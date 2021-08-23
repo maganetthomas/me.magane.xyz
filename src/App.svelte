67e8a73862af9a5a3cac613c51c45e8dc4b01902
Thomas Magane <me@magane@xyz>
2021-08-24 00:31:48 +0200

    initial commit.

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import "./components/Header.svelte"
+</script>
+
+<Header site_name="me.magane.xyz"/>
+
+<style>
+	:global(*, *::before, *::after) {
+		padding: 0;
+		margin: 0;
+		box-sizing: inherit;
+	}
+
+	:global(html, body) {
+		height: 100%;
+		width: 100%;
+	}
+
+	:global(body) {
+		display: grid;
+		place-items: center;
+		box-sizing: border-box;
+	}
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let site_name;
+</script>
+
+<main>
+	<div>{site_name}</div>
+</main>
+
